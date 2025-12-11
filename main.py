3c1b140f14d53b3c91fc55ff923464677d8ea4f6
Renanito69 <renanbascristian@gmail.com>
2025-12-11 03:33:57 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+
+# Menu principal
+def menu_entrada():
+    while True:
+        os.system('cls')
+        print("1 - Cadastro")
+        print("2 - Entrar")
+        print("0 - Sair")
+
+        try:
+            escolha = int(input("\nEscolha: "))
+
+            if escolha == 1:
+                cadastrar_usuario()
+            elif escolha == 2:
+                pass
+            elif escolha == 0:
+                print("Saindo")
+                break
+
+        except ValueError:
+            print("\nERRO!!!")
+            print("Coloque apenas numeros")
+            input("precione alguma tecla para voltar para o menu")
+
+# Responsavel por cadastrar um usuario novo
+def cadastrar_usuario():
+    with open("Cadastros.txt", "a") as arquivo:
+        usuario_novo_usuario = str(input("Nome de Usuario: "))
+        senha_novo_usuario = str(input("Nova Senha: "))
+        confirmacao_senha_usuario = str(input("Confirma Senha: "))
+        if confirmacao_senha_usuario == senha_novo_usuario:
+            arquivo.write(f"{usuario_novo_usuario};{senha_novo_usuario}" + '\n')
+        else:
+            print("Senhas não parecidas")
+
+
+
+
+#Programa inicial
+menu_entrada()
